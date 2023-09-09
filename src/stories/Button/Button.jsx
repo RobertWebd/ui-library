5a74d510d869eb07e65f7a8a92ef47e838c94270
RobertWebd <ra.20.z02@mail.ru>
2023-09-09 14:34:42 +0300

    feat: implemented button component

---
+++ src/stories/Button/Button.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import './Button.css';
+
+export const Button = ({ className, variant, color, content, icon, disabled = false, size, ...props }) => {
+  return (
+    <button className={`button ${className} ${variant} ${color} ${size}`} disabled={disabled} {...props}>
+      <div className="content">
+        {icon}
+        {content}
+      </div>
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  className: '',
+  variant: 'filled',
+  color: 'primary',
+  content: 'dddd',
+  disabled: false,
+  size: 'm',
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['filled', 'outlined']),
+  color: PropTypes.oneOf(['primary', 'secondary']),
+  content: PropTypes.string,
+  icon: PropTypes.node,
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['s', 'm', 'l']),
+};
---
+++ src/stories/Button/Button.stories.js
@@ -0,0 +1,42 @@
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Filled = {
+  args: {
+    content: 'Filled',
+  },
+};
+
+export const Outlined = {
+  args: {
+    variant: 'outlined',
+    content: 'Outlined',
+  },
+};
+
+// export const Secondary = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+
+// export const Large = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+
+// export const Small = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
